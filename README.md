e752f29f16992a068fa103f78aefdcf8a753ee80
Aurélien Devaux <gitlab.aurelearn@aureldvx.net>
2025-03-20 15:01:52 +0100

    chore: configure prettier and eslint

---
+++ README.md
@@ -0,0 +1,1 @@
+npm install -D npm-check-updates @aureldvx/eslint @aureldvx/prettier @aureldvx/tsconfig eslint prettier
---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "@aureldvx/eslint";
+
+export default defineConfig({
+  plugins: {
+    typescript: true,
+    vitest: true,
+  },
+  ignore: ["eslint.config.js", "prettier.config.js"],
+});
