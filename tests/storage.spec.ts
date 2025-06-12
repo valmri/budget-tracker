e1e8b4b49e7dbe5699e7bf945e9070e6a6feb86a
Aurélien Devaux <gitlab.aurelearn@aureldvx.net>
2025-06-12 16:47:33 +0200

    test: delete, update and find transactions

--- tests/storage.spec.ts
+++ tests/storage.spec.ts
@@ -33,19 +33,77 @@ describe('storage', () => {
 			operatedAt: new Date(),
 		} as const;
 		const today = new Date();
+		const startLength = storage.transactions.length;
+
 		vi.setSystemTime(today);
 
 		// 2. Act
-		storage.create(payload);
+		const output = storage.create(payload);
 
 		// 3. Assert
 		expect(storage.transactions.length).toBe(1);
 		expect(storage.transactions.at(0)).toBeInstanceOf(Transaction);
-		expect(storage.transactions.at(0)?.getLabel()).toBe(payload.label);
-		expect(storage.transactions.at(0)?.getAmount()).toBe(payload.amount);
-		expect(storage.transactions.at(0)?.getCurrency()).toBe(payload.currency);
-		expect(storage.transactions.at(0)?.getType()).toBe(payload.type);
-		expect(storage.transactions.at(0)?.getOperatedAt()).toBe(payload.operatedAt);
-		expect(storage.transactions.at(0)?.getCreatedAt()).toStrictEqual(today);
+		expect(storage.transactions[0]).toStrictEqual(output);
+		expect(storage.transactions).toHaveLength(startLength + 1);
+	});
+
+	it('should update an existing transaction', ({ expect }) => {
+		// 1. Arrange
+		const storage = new InMemoryStorage();
+		const transaction = storage.create({
+			label: 'transaction',
+			amount: 9,
+			currency: 'EUR',
+			type: 'expense',
+			operatedAt: new Date(),
+		});
+		const startLength = storage.transactions.length;
+		const updatedLabel = 'nouveau label';
+
+		// 2. Act
+		const output = storage.update(transaction.getId(), {
+			label: updatedLabel,
+		});
+
+		// 3. Assert
+		expect(output?.getLabel()).toStrictEqual(updatedLabel);
+		expect(storage.transactions).toHaveLength(startLength);
+	});
+
+	it('should delete an existing transaction', ({ expect }) => {
+		// 1. Arrange
+		const storage = new InMemoryStorage();
+		const transaction = storage.create({
+			label: 'transaction',
+			amount: 9,
+			currency: 'EUR',
+			type: 'expense',
+			operatedAt: new Date(),
+		});
+		const startLength = storage.transactions.length;
+
+		// 2. Act
+		storage.delete(transaction.getId());
+
+		// 3. Assert
+		expect(storage.transactions).toHaveLength(startLength - 1);
+	});
+
+	it('should find an existing transaction by its id', ({ expect }) => {
+		// 1. Arrange
+		const storage = new InMemoryStorage();
+		const transaction = storage.create({
+			label: 'transaction',
+			amount: 9,
+			currency: 'EUR',
+			type: 'expense',
+			operatedAt: new Date(),
+		});
+
+		// 2. Act
+		const output = storage.show(transaction.getId());
+
+		// 3. Assert
+		expect(output?.getId()).toStrictEqual(transaction.getId());
 	});
 });
